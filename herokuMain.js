ef8a1ce1482e404bdd9826a56f7d453d0ffbb085
vn50fsa <Rodolfo.Saldivar.Hernandez@walmart.com>
2020-05-07 00:51:08 -0500

    heroku deploy test 2

--- herokuMain.js
+++ herokuMain.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const http = require('http');
 const path = require('path');
+const express = require('express');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || '5000';
 app.set('port', port);
 
-const server = http.createServer(app);
-server.listen(port, () => console.log(`Running on localhost:${port}`));
+app.listen(port, () => console.log(`PORT: ${port}`));
