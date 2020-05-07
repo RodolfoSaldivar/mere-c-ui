eb44fec44e11535ae99c3d1442d2978d84f609a5
vn50fsa <Rodolfo.Saldivar.Hernandez@walmart.com>
2020-05-06 22:43:25 -0500

    Create Theme.js

---
+++ src/components/Theme.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+//================================================
+
+const primary = '#9C27B0';
+const secondary = '#4CAF50';
+
+const appTheme = (/*theme*/) =>
+	createMuiTheme({
+		palette: {
+			primary: {
+				main: primary
+			},
+			secondary: {
+				main: secondary
+			},
+			error: {
+				main: '#F44336'
+			},
+			background: {
+				default: '#F5F5F5'
+			}
+		},
+		forAllApp: {
+			'& a': {
+				color: 'inherit',
+				textDecoration: 'none'
+			}
+		}
+	});
+
+//================================================
+
+const Theme = ({ children }) => (
+	<ThemeProvider theme={appTheme(createMuiTheme())}>
+		<CssBaseline />
+		{children}
+	</ThemeProvider>
+);
+
+//================================================
+
+Theme.propTypes = {
+	// From parent <App />
+	children: PropTypes.node.isRequired
+};
+
+export default Theme;
