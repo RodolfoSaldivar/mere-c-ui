1ec9add3a85cc98ef5d52e8568ea1b4b8c4ade9a
vn50fsa <Rodolfo.Saldivar.Hernandez@walmart.com>
2020-05-06 23:11:15 -0500

    Create constants.js

---
+++ src/helpers/constants.js
@@ -0,0 +1,36 @@
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+// Values used across all the app
+
+//================================================
+//----> Strings
+
+export const APP_NAME = 'Mere-C';
+export const DEFAULT_ERROR_MESSAGE = 'App not loading. Please try again in few minutes.';
+
+export const PATHS = {
+	home: '/',
+	users: '/users',
+	groups: '/groups',
+	history: '/history',
+	rewards: '/rewards',
+	requests: '/requests',
+	schedules: '/schedules',
+	permissions: '/permissions'
+};
+
+//================================================
+//----> Booleans
+
+export const IS_IOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+export const useIsMobile = () => useMediaQuery((theme) => theme.breakpoints.down('md'));
+
+//================================================
+//----> Functions
+
+// headerActions - updateNotificationsBadge: "-1" is a negative number.
+export const negativeNumberIn = (actionFileName, functionName, number) => {
+	console.info(
+		`${actionFileName} - ${functionName}:\n"${number}" is a negative number.`
+	);
+};
