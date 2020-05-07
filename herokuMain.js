e5514c18375fd66c8e32cf0b5286b3f91ffd734f
vn50fsa <Rodolfo.Saldivar.Hernandez@walmart.com>
2020-05-07 01:01:11 -0500

    heroku deploy test 4

--- herokuMain.js
+++ herokuMain.js
@@ -6,6 +6,5 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 const port = process.env.PORT || '5000';
-app.set('port', port);
 
 app.listen(port, () => console.log(`PORT: ${port}`));
