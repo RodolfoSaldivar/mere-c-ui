64b3f81b9b668f94c68e6de32572e8e496595dc9
vn50fsa <Rodolfo.Saldivar.Hernandez@walmart.com>
2020-05-09 02:00:50 -0500

    added react cookies

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { CookiesProvider } from 'react-cookie';
 import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 import * as serviceWorker from './serviceWorker';
 
+//================================================
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(reduxThunk)));
 
+//================================================
+
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<CookiesProvider>
+			<App />
+		</CookiesProvider>
 	</Provider>,
 	document.getElementById('root')
 );
